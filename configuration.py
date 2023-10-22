99b090b1c16964bbf82d90406f1a6b4960ec6c41
SokDI93 <145707321+SokDI93@users.noreply.github.com>
2023-10-22 19:38:07 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Соколовский Дмитрий, 9-я когорта - Дипломный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://287cd097-cab5-423e-afa3-68952cc1203e.serverhub.praktikum-services.ru/"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
